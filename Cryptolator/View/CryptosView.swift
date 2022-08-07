a21240f44b27f25735168e7d717e1991659b6213
Harry-KNIGHT <eliott.knight@icloud.com>
2022-08-07 17:31:18 +0200

    [FIX] Used geometry reader for keep crypto list in frame

--- Cryptolator/View/CryptosView.swift
+++ Cryptolator/View/CryptosView.swift
@@ -13,64 +13,69 @@ struct CryptosView: View {
 	@ObservedObject public var cryptos = CryptoCurrencyApi()
 
 	var body: some View {
-		ZStack {
-			Rectangle()
-				.fill(backGradient).blur(radius: 40)
-				.ignoresSafeArea(.all)
-			VStack {
-				ZStack(alignment: .center) {
-					CalculatorScreenBackground()
-						.foregroundColor(Color("ScreenBackGround").opacity(0.16))
-						.padding(.horizontal, 15)
-						.frame(maxWidth: .infinity, maxHeight: 500)
-						.overlay {
-							
-							ScrollView(.vertical, showsIndicators: false) {
-								VStack(alignment: .center, spacing: 30) {
-									ForEach(cryptos.cryptoCurrencies, id: \.self) { crypto in
-										NavigationLink(destination: CalculatorView(crypto: crypto)) {
-											HStack(spacing: 5) {
 
-												AsyncCryptoIconView(crypto: crypto)
-												//.colorMultiply(Color("AsyncImageMultiply"))
-												//.colorInvert()
-													.saturation(0)
-													.colorMultiply(.blue)
-												Spacer()
-												Text(crypto.name ?? "")
-													.font(.custom(FontManager.BlackOpsOne.regular, size: 20, relativeTo: .headline))
-													.foregroundColor(Color("TextMainCryptoListColor"))
-												Spacer()
-												Text("$\(String(format: "%.2f", crypto.currentPrice ?? 0))")
-													.font(.custom(FontManager.BlackOpsOne.regular, size: 15, relativeTo: .headline))
-													.foregroundColor(Color("TextMainCryptoListColor"))
+		GeometryReader { geo in
+			ZStack {
 
+				Rectangle()
+					.fill(backGradient).blur(radius: 40)
+					.ignoresSafeArea(.all)
+				VStack {
+					ZStack(alignment: .center) {
+						CalculatorScreenBackground()
+							.foregroundColor(Color("ScreenBackGround").opacity(0.16))
+							.padding(.horizontal, 15)
+							.frame(maxWidth: geo.size.width, maxHeight: geo.size.height)
+							.overlay {
+
+								ScrollView(.vertical, showsIndicators: false) {
+									VStack(alignment: .center, spacing: 30) {
+										ForEach(cryptos.cryptoCurrencies, id: \.self) { crypto in
+											NavigationLink(destination: CalculatorView(crypto: crypto)) {
+												HStack(spacing: 5) {
+
+													AsyncCryptoIconView(crypto: crypto)
+													//.colorMultiply(Color("AsyncImageMultiply"))
+													//.colorInvert()
+														.saturation(0)
+														.colorMultiply(.blue)
+													Spacer()
+													Text(crypto.name ?? "")
+														.font(.custom(FontManager.BlackOpsOne.regular, size: 20, relativeTo: .headline))
+														.foregroundColor(Color("TextMainCryptoListColor"))
+													Spacer()
+													Text("$\(String(format: "%.2f", crypto.currentPrice ?? 0))")
+														.font(.custom(FontManager.BlackOpsOne.regular, size: 15, relativeTo: .headline))
+														.foregroundColor(Color("TextMainCryptoListColor"))
+
+												}
+												.padding(.horizontal, 30)
 											}
-											.padding(.horizontal, 30)
 										}
-									}
-								}.padding(.vertical, 50)
-							}
-							.mask {
-								CalculatorScreenBackground()
-									.frame(height: 495)
-							}
-							CalculatorScreenBorder()
-								.stroke(Color("ScreenBorder"), lineWidth: 2.0)
-								.frame(maxWidth: .infinity, maxHeight: 500)
-						}
+									}.padding(.vertical, 50)
+								}
+								.mask {
+									CalculatorScreenBackground()
+										.frame(maxWidth: geo.size.width, maxHeight: geo.size.height)
+								}
+								CalculatorScreenBorder()
+									.stroke(Color("ScreenBorder"), lineWidth: 2.0)
+									.frame(maxWidth: geo.size.width, maxHeight: geo.size.height)
 
+							}
+
+					}
+					Image("CircleEffectlumire")
+						.resizable()
+						.scaledToFit()
+						.frame(maxWidth: .infinity, maxHeight: 200)
+						.opacity(0.70)
 				}
-				Image("CircleEffectlumire")
-					.resizable()
-					.scaledToFit()
-					.frame(maxWidth: .infinity, maxHeight: 200)
-					.opacity(0.70)
-			}
-			.navigationBarHidden(true)
-			.task {
-				Task {
-					try await cryptos.fetchCrypto()
+				.navigationBarHidden(true)
+				.task {
+					Task {
+						try await cryptos.fetchCrypto()
+					}
 				}
 			}
 		}
